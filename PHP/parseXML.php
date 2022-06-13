121097449d5b5a2bbe0a5f47b99e25ccb53ac0d6
codrinatus <codrinvasiliu@gmail.com>
2022-06-13 22:22:06 +0300

    xml import prototype

---
+++ PHP/parseXML.php
@@ -0,0 +1,18 @@
+<?php
+
+$db = mysqli_connect('localhost', 'root', '', 'accounts');
+
+$xmlfile    = $_GET["myfile"];
+
+$animalinfo      = simplexml_load_file($xmlfile) or die("Error: Cannot create XML object");
+
+$species  =  $animalinfo->species;
+$type =  $animalinfo->type;
+$diet =  $animalinfo->diet;
+$lifespan  =  $animalinfo->lifespan;
+$climate  =  $animalinfo->climate;
+$description  =  $animalinfo->description;
+
+$query = "INSERT INTO animals (species, type, diet, lifespan, climate, description) VALUES('$species', '$type', '$diet', '$lifespan', '$climate', '$description')";
+mysqli_query($db, $query);
+echo "probabil gata";
